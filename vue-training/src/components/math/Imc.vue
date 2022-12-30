66d6f571c96f777b6e22bbb5c0ad9397d48c73f5
christmknd <christmokonda@gmail.com>
2022-12-30 19:17:55 +0100

    Finish fix imc component problems

--- vue-training/src/components/math/Imc.vue
+++ vue-training/src/components/math/Imc.vue
@@ -1,32 +1,25 @@
 <template>
   <h2>IMC</h2>
   <div>
-    <label for="poids">Votre Poids (kg)</label>
-    <input v-model.number="poids" id="poids" type="number" placeholder="indiquer votre poids en kg"> <br/><br/>
+    <form @submit.prevent="imcCalcul()">
     <label for="taille">Votre Taille (en cm)</label>
-    <input v-model.number="taille" id="taille" type="number" placeholder="indiquer votre taille en cm"> <br/><br/>
-    <button @click="imc" id="convert-button" type="submit">Entrer</button>
-    <span><p id="p-result" v-if="imc !== null" > Votre imc est de {{ imc }}</p></span>
-    <!--
- <template v-if="imc < 18.5">
-      <p> Insuffisance pondérale </p>
-    </template>
-    <template v-else-if="18.5 < imc < 25">
-      <p> poids normal </p>
-    </template>
-    <template v-else-if="25 < imc < 30">
-      <p> Surpoids </p>
-    </template>
-    <template v-else>
-      <p> Obèse </p>
-    </template>
-    -->
+    <input v-model.number="taille" id="taille" type="text" placeholder="indiquer votre taille en cm"> <br/><br/>
+    <label for="poids">Votre Poids (kg)</label>
+    <input v-model.number="poids" id="poids" type="text" placeholder="indiquer votre poids en kg"> <br/><br/>
+    <button id="convert-button" type="submit">Entrer</button>
+  </form>
+  <div class="imc-result">
+    <span><p v-if="imc !== null" id="p-result"> Votre imc est de {{ imc }}</p></span>
+      <p v-if="imc < 18.5"> Insuffisance pondérale </p>
+      <p v-else-if="imc >= 18.5 && imc < 25"> poids normal </p>
+      <p v-else-if="imc >= 25 && imc < 30"> Surpoids </p>
+      <p v-else> Obésité </p>
   </div>
-  
+</div>
 </template>
 
 <script>
-import { ref , computed} from 'vue'
+import { ref} from 'vue'
 
 export default {
     name: 'Imc',
@@ -34,19 +27,20 @@ export default {
 
         let taille = ref(0)
         let poids = ref(0)
-
-        let p = poids.value
-        let t = taille.value / 100
-
+        let imc = ref(null)
+      
   
-         let imc = computed(() => {
-          Math.round(p / (Math.pow(t,2)))
-         }) 
+        function imcCalcul () {
+          let p = poids.value
+          let t = taille.value/100
+          return imc.value =  Math.round(p /(Math.pow(t,2)))
+        }
 
         return {
             poids,
             taille,
             imc,
+            imcCalcul
         }
     }
 }
